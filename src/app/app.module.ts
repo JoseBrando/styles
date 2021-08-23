14c8da5bd0ff29aa7e19a0edae6aa33c6ddd675b
Brando <jose.brando.c.m@gmail.com>
2021-08-22 22:46:46 -0500

    [FEAT] Custom circles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CircleComponent } from './components/circle/circle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircleComponent
   ],
   imports: [
     BrowserModule
